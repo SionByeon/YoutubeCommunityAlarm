ad59db948fc7420150ba3c85b291194eb0cce53b
Sion Byeon <sion@bucketplace.net>
2024-03-21 14:49:45 +0900

    crontab_register, method name

--- crontab_register.py
+++ crontab_register.py
@@ -1,4 +1,3 @@
-import argparse
 import subprocess
 import os
 import sys
@@ -25,7 +24,7 @@ class CrontabRegister:
         time_interval_argument = " --time_interval_in_minutes " + self.time_interval_in_minutes if self.time_interval_in_minutes else "--time_interval_in_hours " + self.time_interval_in_hours
         self.arguments = f"--channel_id {self.channel_id} " + time_interval_argument
 
-    def register_cron(self):
+    def register_crontab(self):
         cron_command = f"{self.python_executable} {self.absolute_path_of_python_script} {self.arguments} >> {self.absolute_path_of_log_file} 2>&1"
         cron_job = f'(crontab -l; echo "{self.cron_expression} {cron_command}") | crontab -'
 
@@ -34,28 +33,6 @@ class CrontabRegister:
             print("Cron job registered successfully.")
         except subprocess.CalledProcessError as e:
             print(f"Error registering cron job: {e}")
-
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--channel_id', type=str, help='Channel ID')
-    parser.add_argument('--time_interval_in_minutes', type=str, help='Time interval in minutes')
-    parser.add_argument('--time_interval_in_hours', type=str, help='Time interval in hours')
-
-    args = parser.parse_args()
-    kwargs = {}
-    if args.time_interval_in_minutes is not None:
-        kwargs["time_interval_in_minutes"] = args.time_interval_in_minutes
-    if args.time_interval_in_hours is not None:
-        kwargs["time_interval_in_hours"] = args.time_interval_in_hours
-
-    if args.channel_id is not None:
-        cr = CrontabRegister(channel_id=args.channel_id, **kwargs)
-        cr.register_cron()
-    else:
-        print("Please provide a channel_id.")
-        exit(1)
 
 
 
