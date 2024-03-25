f8c6c6081d4d21a4bf849780d78f6b8565faa9e9
Sion Byeon <sion@bucketplace.net>
2024-03-25 14:55:19 +0900

    version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="youtube-community-alarm",
-    version="0.0.1",
+    version="0.0.2",
     author="palace",
     author_email="ufo3764@naver.com",
     description="youtube community alarm package",
